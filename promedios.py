7b20670768ddfb89efb61ca499349a7a9b981087
teobotta <teombotta@gmail.com>
2018-05-09 20:34:58 -0300

    Subo la versión 1.0

---
+++ promedios.py
@@ -0,0 +1,33 @@
+# Developer: Botta, Teo Mariano
+
+# Versión: 1.0
+
+#El siguiente programa realiza un promedio de las notas ingresadas por teclado. Las cuales pueden ser una cantidad indefinida.
+
+
+
+import os	#Importo librería que permite función de limpiado de pantalla
+
+os.system('clear')	#Limpio la consola al comienzo de la ejecución
+p = 0	#Inicializo variable de acumulación 
+l = []	#Inicializo una lista en la cual se irán ingresando las notas a promediar
+r = input('Agregar nota? (s/n): ')	#Se solicita confirmación inicial a la carga de notas
+while(r == 's'):	#Se realiza la iteración mientras la variable 'r' sea igual a 's'
+	os.system('clear')	#Limpi pantalla
+	n = input('Ingrese nota: ')	#Solicito nota
+	l.append(n)	#Agrego la nota al final de la lista 'l'
+	print('-----------------------')
+	r = input('Agregar otra nota? (s/n): ')	#Se solicita confirmación de continuidad
+c = 0	#Inicializo una variable contador
+os.system('clear')	#Limpio pantalla
+for x in l:	#Recorro todos los elementos de la lista
+	p = p + int(x)	#Voy sumando los valores de cada nota en la variable 'p'
+	c = c + 1	#Aumento la variable contador
+if(c != 0):
+	p = p / c 	#Realizo el promedio de las notas y las guardo en la variable 'p'
+	print('El promedio de las notas es de: ',float(p))	#Imprimo en consola el promedio
+else: 
+	print('No hay notas cargadas.')	#Muestro mensaje de ausencia de datos
+print('-------------------------------------')
+input('Presione cualquier tecla para finalizar...')	#Solicito presión de cualquier tecla antes de finalizar
+os.system('clear')	#Limpio pantalla antes de finalizar
