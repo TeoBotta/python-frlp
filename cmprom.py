7a90ed24f95baaa6ed0db45d1d9c21c8a4b4883d
teobotta <teombotta@gmail.com>
2018-05-12 19:48:11 -0300

    Actualizo aplicación 'promedios.py' a versión 2.0

---
+++ cmprom.py
@@ -0,0 +1,11 @@
+class Materia():
+		"""docstring for Materia"""
+		def __init__(self, n1, n2):
+			self.nombre = n1
+			self.nota = n2
+
+		def verNombre(self):
+			return self.nombre
+
+		def verNota(self):
+			return self.nota
--- promedios.py
+++ promedios.py
@@ -7,6 +7,7 @@
 
 
 import os	#Importo librería que permite función de limpiado de pantalla
+from cmprom import * #Invoco librería 'cmprom' para utilizar la clase 'Materia'
 
 os.system('clear')	#Limpio la consola al comienzo de la ejecución
 p = 0	#Inicializo variable de acumulación 
@@ -14,15 +15,22 @@ l = []	#Inicializo una lista en la cual se irán ingresando las notas a promedia
 r = input('Agregar nota? (s/n): ')	#Se solicita confirmación inicial a la carga de notas
 while(r == 's'):	#Se realiza la iteración mientras la variable 'r' sea igual a 's'
 	os.system('clear')	#Limpi pantalla
+	m = input('Ingrese materia: ')	#Solicito materia
+	print('')
 	n = input('Ingrese nota: ')	#Solicito nota
-	l.append(n)	#Agrego la nota al final de la lista 'l'
+	mate = Materia(m,n)	#Instancio una clase de tipo 'Materia'
+	l.append(mate)	#Agrego la instancia 'mate' al final de la lista 'l'
 	print('-----------------------')
 	r = input('Agregar otra nota? (s/n): ')	#Se solicita confirmación de continuidad
 c = 0	#Inicializo una variable contador
 os.system('clear')	#Limpio pantalla
+print('Listado de notas:')
 for x in l:	#Recorro todos los elementos de la lista
-	p = p + int(x)	#Voy sumando los valores de cada nota en la variable 'p'
+	p = p + int(x.verNota())	#Voy sumando los valores de cada nota en la variable 'p'
 	c = c + 1	#Aumento la variable contador
+	print('Materia: '+ str(x.verNombre()))	#Imprimo valores invocando funciones de la clase 'Materia' 
+	print('Nota: '+ str(x.verNota()))	#Imprimo valores invocando funciones de la clase 'Materia'
+	print('----------------------------------')
 if(c != 0):
 	p = p / c 	#Realizo el promedio de las notas y las guardo en la variable 'p'
 	print('El promedio de las notas es de: ',float(p))	#Imprimo en consola el promedio
