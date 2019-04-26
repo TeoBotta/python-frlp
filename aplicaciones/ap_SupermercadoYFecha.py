4174162827e4db6d72cf854db746a820af277126
juanpablomoscoloni <juampi9611@gmail.com>
2019-04-26 10:35:51 -0300

    subiendo tp numero 2

--- aplicaciones/ap_SupermercadoYFecha.py
+++ aplicaciones/ap_SupermercadoYFecha.py
@@ -5,38 +5,43 @@ from datetime import datetime, date, time
 
 #Crea el supermercado
 super=crearSupermercado()
-
+formato = "%d/%m/%Y"
 #Cargar el curso
-rta = True
+rta = 's'
 while rta == 's':
 	p=crearProducto()
-	n=input("Ingrese un codigo ")
-	l=input("Ingrese un precio ")
-	p=input("Ingrese un nombre ")
+	c=int(input("Ingrese un codigo "))
+	l=int(input("Ingrese un precio "))
+	nom=input("Ingrese un nombre ")
 	isValidDate = False
-	while isValidDate = False:
-		fecha=input("Ingrese fecha de vencimiento del producto. Formato: dd/mm/yyyy")
-		day,month,year = fecha.split('/')
-		if len(year) == 4 and len(month) == 2 and len(day) == 2:
-			fecha = date(int(year),int(month),int(day))
-			isValidDate = True
+	while isValidDate == False:
+		try:
+			fecha=input("Ingrese fecha de vencimiento del producto. Formato: dd/mm/yyyy: ")
+			day,month,year = fecha.split('/')
+			if len(year) == 4 and len(month) == 2 and len(day) == 2 and int(year) > 2000 and int(year) < 2050:
+				fecha = date(int(year),int(month),int(day))
+				isValidDate = True
 
-	
-	cargarProducto(p,n,l,p,fecha)
+		except ValueError:
+			print("Fecha incorrecta. Ingresela nuevamente.")
+			isValidDate = False
+
+
+
+	cargarProducto(p,c,l,nom,datetime.strftime(fecha, formato))
 	agregarProducto(super,p)
 	rta = input("Ingresa otro alumno?s/n")
 
-#Imprime los productos vencidos  
+#Imprime los productos vencidos
 
 tam=tamanio(super)
 print("Los productos vencidos son:")
 for i in range(0,tam):
-    a=recuperarProducto(super,i)
-	if verVto(i) < datetime.today)
-		print verCodigo(p)
-		print verPrecio(p)
-		print verNombre(p)
-		print verFecha(p)
-    print '__________'*3
+	a=recuperarProducto(super,i)
+	fechaActual= datetime.strptime(verVto(a), formato)
+	if fechaActual < datetime.today():
 
-
+		print(verCodigo(p))
+		print(verPrecio(p))
+		print(verNombre(p))
+	print('__________'*3)
